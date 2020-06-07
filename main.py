ddf7e42c6f4bff7cdb703f2ce9f86b0ad135f53f
NamH <namnh0122@gmail.com>
2020-06-07 11:08:20 +0700

    Update
    Signed-off-by: NamH <namnh0122@gmail.com>

--- main.py
+++ main.py
@@ -1,8 +1,10 @@
 import firebase_admin
 import google.cloud
-from firebase_admin import credentials, firestore
+from firebase_admin import credentials, firestore,storage
 import json
 import xlrd
+from PIL import Image
+from pathlib import Path
 
 class FirestorePush:
     def __init__(self):
@@ -10,11 +12,273 @@ class FirestorePush:
         self.app=firebase_admin.initialize_app(cred)
         self.store=firestore.client()
 
-        self.loc=('learnenglish_data.xlsx')
+        self.loc=('learnenglish_data_new.xlsx')
         self.wb=xlrd.open_workbook(self.loc)
 
-        # with open('./data-p5.json','r') as f:
-            # self.data = json.load(f)
+        self.bucket=storage.bucket('junes-7b38b.appspot.com')
+        self.res1='./res1/'
+        self.res2='./res2/'
+        self.res3='./res3/'
+        self.res4='./res4/'
+    
+    def insert_part_4(self):
+        question_ref=self.store.collection('questions')
+        sheet=self.wb.sheet_by_name('Part4')
+        current_row=1
+        total_row=sheet.nrows
+
+        while (current_row<total_row):
+            question_id = sheet.cell_value(current_row,1).strip()
+            if len(question_id) == 0:
+                question_id = question_ref.document().id
+
+            question_index = int(sheet.cell_value(current_row,0))
+
+            audio_path = "{}{}.mp3".format(self.res4, question_index)
+            self.upload_audio(audio_path, question_id)
+
+            self.insert_explaination(sheet.cell_value(current_row, 23), sheet.cell_value(current_row,24), question_id)
+
+            image_path = "{}{}.jpg".format(self.res4, question_index)
+            remote_image_path=self.upload_image(image_path, question_id)
+            data = {}
+            if remote_image_path is None:
+                data = {
+                    'a': question_id+'.mp3',
+                    'cC': 0, # comment count
+                    'eC': 1, # explaination count
+                    'q': [
+                        {
+                            'c': sheet.cell_value(current_row, 8),
+                            'q': sheet.cell_value(current_row, 2),
+                            's': sheet.cell_value(current_row, 7)
+                        },
+                        {
+                            'c': sheet.cell_value(current_row, 15),
+                            'q': sheet.cell_value(current_row, 9),
+                            's': sheet.cell_value(current_row, 14)
+                        },
+                        {
+                            'c': sheet.cell_value(current_row, 22),
+                            'q': sheet.cell_value(current_row, 16),
+                            's': sheet.cell_value(current_row, 21)
+                        },
+                    ],
+                    'qId': question_id,
+                    't': 'toeic-p4'
+                }
+            else:
+                data = {
+                    'a': question_id+'.mp3',
+                    'cC': 0, # comment count
+                    'eC': 1, # explaination count
+                    'i': remote_image_path,
+                    'q': [
+                        {
+                            'c': sheet.cell_value(current_row, 8),
+                            'q': sheet.cell_value(current_row, 2),
+                            's': sheet.cell_value(current_row, 7)
+                        },
+                        {
+                            'c': sheet.cell_value(current_row, 15),
+                            'q': sheet.cell_value(current_row, 9),
+                            's': sheet.cell_value(current_row, 14)
+                        },
+                        {
+                            'c': sheet.cell_value(current_row, 22),
+                            'q': sheet.cell_value(current_row, 16),
+                            's': sheet.cell_value(current_row, 21)
+                        },
+                    ],
+                    'qId': question_id,
+                    't': 'toeic-p4'
+                }
+            question_ref.document(question_id).set(data)
+            print('qId: '+question_id)
+            current_row+=1
+        print('Finished')
+
+    def insert_part_3(self):
+        question_ref=self.store.collection('questions')
+        sheet=self.wb.sheet_by_name('Part3')
+        current_row=1
+        total_row=sheet.nrows
+
+        while (current_row<total_row):
+            question_id = sheet.cell_value(current_row,1).strip()
+            if len(question_id) == 0:
+                question_id = question_ref.document().id
+
+            question_index = int(sheet.cell_value(current_row,0))
+
+            audio_path = "{}{}.mp3".format(self.res3, question_index)
+            self.upload_audio(audio_path, question_id)
+
+            self.insert_explaination(sheet.cell_value(current_row, 23), sheet.cell_value(current_row,24), question_id)
+
+            image_path = "{}{}.jpg".format(self.res3, question_index)
+            remote_image_path=self.upload_image(image_path, question_id)
+            data = {}
+            if remote_image_path is None:
+                data = {
+                    'a': question_id+'.mp3',
+                    'cC': 0, # comment count
+                    'eC': 1, # explaination count
+                    'q': [
+                        {
+                            'c': sheet.cell_value(current_row, 8),
+                            'q': sheet.cell_value(current_row, 2),
+                            's': sheet.cell_value(current_row, 7)
+                        },
+                        {
+                            'c': sheet.cell_value(current_row, 15),
+                            'q': sheet.cell_value(current_row, 9),
+                            's': sheet.cell_value(current_row, 14)
+                        },
+                        {
+                            'c': sheet.cell_value(current_row, 22),
+                            'q': sheet.cell_value(current_row, 16),
+                            's': sheet.cell_value(current_row, 21)
+                        },
+                    ],
+                    'qId': question_id,
+                    't': 'toeic-p3'
+                }
+            else:
+                data = {
+                    'a': question_id+'.mp3',
+                    'cC': 0, # comment count
+                    'eC': 1, # explaination count
+                    'i': remote_image_path,
+                    'q': [
+                        {
+                            'c': sheet.cell_value(current_row, 8),
+                            'q': sheet.cell_value(current_row, 2),
+                            's': sheet.cell_value(current_row, 7)
+                        },
+                        {
+                            'c': sheet.cell_value(current_row, 15),
+                            'q': sheet.cell_value(current_row, 9),
+                            's': sheet.cell_value(current_row, 14)
+                        },
+                        {
+                            'c': sheet.cell_value(current_row, 22),
+                            'q': sheet.cell_value(current_row, 16),
+                            's': sheet.cell_value(current_row, 21)
+                        },
+                    ],
+                    'qId': question_id,
+                    't': 'toeic-p3'
+                }
+            question_ref.document(question_id).set(data)
+            print(question_id)
+            current_row+=1
+        print('Finished')
+
+    def insert_part_2(self):
+        question_ref=self.store.collection('questions')
+        sheet=self.wb.sheet_by_name('Part2')
+        current_row=251
+
+        total_row=sheet.nrows
+        while (current_row<total_row):
+            question_id = sheet.cell_value(current_row,1).strip()
+            if len(question_id) == 0:
+                question_id = question_ref.document().id
+
+            correct_answer = sheet.cell_value(current_row,2).strip()
+            question_index = int(sheet.cell_value(current_row,0))
+            audio_path = "{}{}.mp3".format(self.res2, question_index)
+            self.upload_audio(audio_path, question_id)
+
+            self.insert_explaination(sheet.cell_value(current_row, 3), sheet.cell_value(current_row,4), question_id)
+            data = {
+                'a': question_id+'.mp3',
+                'cC': 0, # comment count
+                'eC': 1, # explaination count
+                'q': [
+                    {
+                        'c': correct_answer
+                    }
+                ],
+                'qId': question_id,
+                't': 'toeic-p2'
+            }
+            question_ref.document(question_id).set(data)
+            print(question_id)
+            current_row+=1
+        print('Finished insert part 2')
+
+    def insert_part_1(self):
+        question_ref=self.store.collection('questions')
+        sheet=self.wb.sheet_by_name('Part1')
+        current_row=61
+        total_row=sheet.nrows
+        while (current_row<total_row):
+            question_id = sheet.cell_value(current_row,1).strip()
+            if len(question_id) == 0:
+                question_id = question_ref.document().id
+
+            correct_answer = sheet.cell_value(current_row,2).strip()
+            question_index = int(sheet.cell_value(current_row,0))
+
+            audio_path = "{}{}.mp3".format(self.res1, question_index)
+            image_path = "{}{}.jpg".format(self.res1, question_index)
+
+            self.upload_audio(audio_path, question_id)
+            remote_image_path=self.upload_image(image_path, question_id)
+
+            self.insert_explaination(sheet.cell_value(current_row, 3), sheet.cell_value(current_row,4), question_id)
+            
+            data = {
+                'a': question_id+'.mp3',
+                'cC': 0, # comment count
+                'eC': 1, # explaination count
+                'i': remote_image_path,
+                'q': [
+                    {
+                        'c': correct_answer
+                    }
+                ],
+                'qId': question_id,
+                't': 'toeic-p1'
+            }
+            
+            question_ref.document(question_id).set(data)
+            print(question_id)
+            current_row+=1
+        print('Finished insert part 1')
+
+    def upload_image(self, image_path, question_id):
+        my_file = Path(image_path)
+        if my_file.is_file():
+            img = Image.open(image_path)
+            width, height = img.size
+
+            remote_image_name='%s_%s_%s.jpg'%(question_id, width, height)
+            imageBlob = self.bucket.blob('question_resources/'+remote_image_name)
+            imageBlob.upload_from_filename(image_path, content_type='image/jpg')
+
+            return remote_image_name
+        else:
+            return None
+    
+    def upload_audio(self, audio_local_path, question_id):
+        audio_blob=self.bucket.blob('question_resources/%s.mp3'%question_id)
+        audio_blob.upload_from_filename(audio_local_path, content_type='audio/mpeg')
+
+    def insert_explaination(self, eId, explaination, question_id):
+        explaination_ref=self.store.collection('explainations')
+        if len(eId) == 0:
+            eId=explaination_ref.document().id
+        data = {
+            'eId': eId,
+            'qId': question_id,
+            't': 'practice', # type for practice question
+            'e': explaination
+        }
+        explaination_ref.document(eId).set(data)
+        print('eId: {}'.format(eId))
 
     def add_explaination_p5(self):
         explaination_ref=self.store.collection('explainations')
@@ -172,8 +436,7 @@ class FirestorePush:
 
 if __name__ == '__main__':
     f = FirestorePush()
-    f.add_explaination_p5()
-    # f.read_excel()
+    f.insert_part_2()
+    # f.add_explaination_p5()
     # f.test_upload_p6()
     #f.my_push()
-
